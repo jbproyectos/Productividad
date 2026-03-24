263fd1717ae6d5874482b2494a7d08e087789b5f
Gustavo Sanchez <gs.proyectos@grupo-cise.com>
2026-03-24 12:55:56 -0600

    Filter subcategories by active status in query

--- tickets/get_subcategorias-form.php
+++ tickets/get_subcategorias-form.php
@@ -2,7 +2,7 @@
 include '../includes/conexionbd.php';
 $area_id = $_GET['area_id'];
 $categoria_id = $_GET['categoria_id'];
-$stmt = $pdo->prepare("SELECT id, nombre_sucat FROM subcategorias_ticket WHERE id_area = ? AND id_categoria = ? ORDER BY nombre_sucat");
+$stmt = $pdo->prepare("SELECT id, nombre_sucat FROM subcategorias_ticket WHERE id_area = ? AND id_categoria = ? AND estatus_activo_sub = 'ACTIVO' ORDER BY nombre_sucat");
 $stmt->execute([$area_id, $categoria_id]);
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 ?>
