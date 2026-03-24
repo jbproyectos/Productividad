dcb62fb6f308010fd44d39ff5a3020e81c1abdd6
Gustavo Sanchez <gs.proyectos@grupo-cise.com>
2026-03-24 12:55:21 -0600

    Filter detalle_donde_ticket by active status

--- tickets/get_detalle_donde.php
+++ tickets/get_detalle_donde.php
@@ -2,7 +2,7 @@
 include '../includes/conexionbd.php';
 $area_id = $_GET['area_id'];
 $donde_id = $_GET['donde_id'];
-$stmt = $pdo->prepare("SELECT id, nombre FROM detalle_donde_ticket WHERE id_area = ?  ORDER BY nombre");
+$stmt = $pdo->prepare("SELECT id, nombre FROM detalle_donde_ticket WHERE id_area = ? AND estatus_activo_det_donde = 'ACTIVO' ORDER BY nombre");
 $stmt->execute([$area_id]);
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 ?>
