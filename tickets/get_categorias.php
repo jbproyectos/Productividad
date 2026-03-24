ff0db8f428a18e2f002c198a8470f969e576fd0e
Gustavo Sanchez <gs.proyectos@grupo-cise.com>
2026-03-24 12:54:54 -0600

    Filter categories by active status in SQL query

--- tickets/get_categorias.php
+++ tickets/get_categorias.php
@@ -1,7 +1,7 @@
 <?php
 include '../includes/conexionbd.php';
 $area_id = $_GET['area_id'];
-$stmt = $pdo->prepare("SELECT id, nombre_cat FROM categoria_servicio_ticket WHERE id_area = ? ORDER BY nombre_cat");
+$stmt = $pdo->prepare("SELECT id, nombre_cat FROM categoria_servicio_ticket WHERE id_area = ? AND estatus_activo_cat = 'ACTIVO' ORDER BY nombre_cat");
 $stmt->execute([$area_id]);
 echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 ?>
